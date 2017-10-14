3e32b341a1dfe076d1a37ee5ad7da14e8883e1a3
SMITHsharon <sharon.e.smith2@gmail.com>
2017-10-14 09:57:23 -0700

    scaffolded MVC for Song object / writing to browser

---
+++ RazorPractice/Models/Song.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RazorPractice.Models
+{
+    public class Song
+    {   
+        [Required]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public string Album { get; set; }
+        public string Genre { get; set; }
+    }
+}
